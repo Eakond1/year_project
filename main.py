55827f68caf063237483aae0696bc615e304e7af
Kate <143996703+Eakond1@users.noreply.github.com>
2024-02-20 15:15:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,18 +3,12 @@ import openpyxl
 import telebot
 from telebot import types
 import json
-import threading
-import calendar
-from config import tlist
-from datetime import datetime
+import datetime
+import schedule
 bot = telebot.TeleBot('')
-lessons = []
 users = []
 
 try:
-    with open('lessons.json', 'r', encoding='utf-8') as file:
-        lessons = json.loads(file.read())
-
     with open('users.json', 'r', encoding='utf-8') as file:
         users = json.loads(file.read())
 except Exception as e:
@@ -22,8 +16,6 @@ except Exception as e:
 
 
 def save_data():
-    with open('lessons.json', 'w', encoding='utf-8') as file:
-        file.write(json.dumps(lessons, ensure_ascii=False))
     with open('users.json', 'w', encoding='utf-8') as file:
         file.write(json.dumps(users, ensure_ascii=False))
 
@@ -31,6 +23,7 @@ def save_data():
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
     chat_id = message.chat.id
+
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     class_10t = types.KeyboardButton("10Т")
     class_10i = types.KeyboardButton("10И")
@@ -42,45 +35,34 @@ def start(message):
 def know_user_class(message):
     chat_id = message.chat.id
     user_class = message.text
-    if user_class == "10Т" or user_class == "10И":
-        users.append({
-            "id": chat_id,
-            "class": user_class
-        })
-        save_data()
-        bot.send_message(chat_id, 'Вы успешно завершили регистрацию', reply_markup=types.ReplyKeyboardRemove())
+    u = find_user(chat_id)
+    if u == True:
+        change_user_class(chat_id, user_class)
+        bot.send_message(chat_id, 'Вы успешно изменили свой класс', reply_markup=types.ReplyKeyboardRemove())
     else:
-        m = bot.send_message(chat_id, 'Повторите попытку')
-        bot.register_next_step_handler(m, know_user_class)
+        if user_class == "10Т" or user_class == "10И":
+            users.append({
+                "id": chat_id,
+                "class": user_class
+            })
+            save_data()
+            bot.send_message(chat_id, 'Вы успешно завершили регистрацию', reply_markup=types.ReplyKeyboardRemove())
+        else:
+            m = bot.send_message(chat_id, 'Повторите попытку')
+            bot.register_next_step_handler(m, know_user_class)
 
 
 @bot.message_handler(commands=['schedule'])
 def start(message):
     chat_id = message.chat.id
-    lesson = get_list_of_lessons("Monday", tlist[0]['class'] )
-    bot.send_message(chat_id, 'Расписание на понедельник:')
-    send_lessons(lesson, chat_id)
+    schedule.every().day.at("15:13").do(send_notification, chat_id)
 
-    lesson = get_list_of_lessons("Tuesday",  tlist[0]['class'])
-    bot.send_message(chat_id, 'Расписание на вторник')
-    send_lessons(lesson, chat_id)
-
-    lesson = get_list_of_lessons("Wednesday",  tlist[0]['class'])
-    bot.send_message(chat_id, 'Расписание на среду')
-    send_lessons(lesson, chat_id)
-
-    lesson = get_list_of_lessons("Thursday",  tlist[0]['class'])
-    bot.send_message(chat_id, 'Расписание на четверг')
-    send_lessons(lesson, chat_id)
-
-    lesson = get_list_of_lessons("Friday",  tlist[0]['class'])
-    bot.send_message(chat_id, 'Расписание на пятницу')
-    send_lessons(lesson, chat_id)
 
 def send_lessons(lesson, chat_id):
     for i in range(len(lesson)):
         bot.send_message(chat_id, lesson[i])
         time.sleep(1)
+
 
 def get_list_of_lessons(day, user_class):
     if user_class == "10Т":
@@ -141,9 +123,67 @@ def get_list_of_lessons(day, user_class):
         return lesson
 
 
+def find_user(id):
+    for u in users:
+        if u['id'] == id:
+            return True
+    return False
 
 
+def get_user_class(id):
+    for u in users:
+        if u['id'] == id:
+            return u['class']
 
 
+def change_user_class(id, new_class):
+    for u in users:
+        if u['id'] == id:
+            u['class'] = new_class
+            save_data()
 
-bot.polling()
+
+def send_notification(chat_id):
+    print(chat_id)
+    wd = datetime.datetime.today().weekday()
+    if wd == 0:
+        clas = get_user_class(chat_id)
+        lesson = get_list_of_lessons("Monday", clas)
+        bot.send_message(chat_id, 'Расписание на понедельник:')
+        send_lessons(lesson, chat_id)
+    elif wd == 1:
+        clas = get_user_class(chat_id)
+        lesson = get_list_of_lessons("Tuesday", clas)
+        bot.send_message(chat_id, 'Расписание на вторник')
+        send_lessons(lesson, chat_id)
+    elif wd == 2:
+        clas = get_user_class(chat_id)
+        lesson = get_list_of_lessons("Wednesday", clas)
+        bot.send_message(chat_id, 'Расписание на среду')
+        send_lessons(lesson, chat_id)
+    elif wd == 3:
+        clas = get_user_class(chat_id)
+        lesson = get_list_of_lessons("Thursday", clas)
+        bot.send_message(chat_id, 'Расписание на четверг')
+        send_lessons(lesson, chat_id)
+    elif wd == 4:
+        clas = get_user_class(chat_id)
+        lesson = get_list_of_lessons("Friday", clas)
+        bot.send_message(chat_id, 'Расписание на пятницу')
+        send_lessons(lesson, chat_id)
+
+
+def run_bot():
+    bot.polling(none_stop=True)
+
+
+def run_schedule():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    import threading
+    threading.Thread(target=run_bot).start()
+    threading.Thread(target=run_schedule).start()
